89c12e46016539455eca9e3ee137d74bef82611d
Diego Sousa <diego7lts7@gmail.com>
2024-10-29 16:48:49 -0300

    criando as rotas do incio.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Router>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    <Routes>
+      {/* <Route path="/" element={}/>
+      <Route path="/login" element={}/>
+      <Route path="/register" element={}/>
+      <Route path="/recover-password" element={}/> */}
+    </Routes>
+  </Router>
 );
